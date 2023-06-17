f596aa70a58bac58a3bf6e44f08f2bfc961dd584
Alawode <alawodeolayinka2@gmail.com>
2023-06-16 17:11:33 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
 
 /**
-  * main - Prints the numbers from 00 to 99
+  * main - Prints 3 combination of numbers
   *
   * Return: Always (Success)
   */
 int main(void)
 {
-	int c, i;
-
-	c = i = '0';
+	int c, i, k;
 
 	for (c = '0'; c <= '9'; c++)
 	{
 		for (i = '0'; i <= '9'; i++)
 		{
-			putchar(c);
-			putchar(i);
-
-			if ((c != '9') || (c == '9' && i != '9'))
+			for (k = '0'; k <= '9'; k++)
 			{
-				putchar(',');
-				putchar(' ');
+				if (c < i && i < k)
+				{
+					putchar(c);
+					putchar(i);
+					putchar(k);
+
+					if (c != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
 			}
 		}
 	}

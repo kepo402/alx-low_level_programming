7a45be277b6a32c6a3985c9a02ec7b56620c0db2
kepo402 <alawodeolayinka2@gmail.com>
2024-03-25 10:17:29 -0700

    0x18-dynamic_libraries

---
+++ 0x18-dynamic_libraries/nrandom.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * add - Returns 1.
+ * @a: First integer.
+ * @b: Second integer.
+ *
+ * Return: Always 1.
+ */
+int add(int a, int b)
+{
+    return 1; // Always return 1
+}
+
+/**
+ * main - Entry point.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    return 0;
+}

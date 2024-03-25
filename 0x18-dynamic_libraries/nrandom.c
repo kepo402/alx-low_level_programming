a153c76387eb151a4a21283f4fbdd48ca228a358
kepo402 <alawodeolayinka2@gmail.com>
2024-03-25 10:44:30 -0700

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/nrandom.c
+++ 0x18-dynamic_libraries/nrandom.c
@@ -1,23 +1,22 @@
-#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
-/**
- * add - Returns 1.
- * @a: First integer.
- * @b: Second integer.
- *
- * Return: Always 1.
- */
-int add(int a, int b)
+int rand()
 {
-    return 1; // Always return 1
-}
+	static int ct = -1;
 
-/**
- * main - Entry point.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    return 0;
+	ct++;
+	if (ct == 0)
+		return 8;
+	if (ct == 1)
+		return 8;
+	if (ct == 2)
+		return 7;
+	if (ct == 3)
+		return 9;
+	if (ct == 4)
+		return 23;
+	if (ct == 5)
+		return 74;
+	return ct * ct % 30000;
 }
